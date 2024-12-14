c1fd805fb54cd224902d94cd325bc26ff831eef6
Karishma271 <karishmapatel273@gmail.com>
2024-12-13 20:53:01 -0500

    add:

--- routes/venue.js
+++ routes/venue.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Venue = require('../models/Venue');
-const { body, validationResult } = require('express-validator');
+
 
 /**
  * Route to get all venues
